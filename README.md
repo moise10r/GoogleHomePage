de8cb3ade84e32a5cbc6de1ad351f2dad45afbe0
moise10r <moiserushanika2006@gmail.com>
2021-05-24 11:25:33 +0200

    refactor the css code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Google-Homepage
+this is NGANULO RUSHANIKA Moise.
 I recreated the Google Homepage
+
+# Structure 
+this project is well designed ,easy to understand .
+
 
---
+++ js/index.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("input");
+const body = document.querySelector("body");
+input.addEventListener("click", () => {
+	// if (input.className !== "hide") {
+	input.classList.add("hide");
+	// }
+	// input.classList.remove("hide");
+});
